578af3b47322bfe3bf195d715065d1bdd0f73c9b
Anastasiia Rubanova <rubanova.n10@gmail.com>
2025-10-24 14:10:36 +0300

    first commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import SearchBar from "./SearchBar/SearchBar";
-import MovieGrid from "./MovieGrid/MovieGrid";
-import MovieModal from "./MovieModal/MovieModal";
-import Loader from "./Loader/Loader";
-import ErrorMessage from "./ErrorMessage/ErrorMessage";
-import { fetchMovies } from "../services/movieService";
-import type{ Movie } from "../types/movie";
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import MovieModal from "../MovieModal/MovieModal";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import { fetchMovies } from "../../services/movieService";
+import type{ Movie } from "../../types/movie";
 import { Toaster } from "react-hot-toast";
 
 export default function App() {
